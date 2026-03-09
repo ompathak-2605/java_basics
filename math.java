e64f21d70555c2bc742fe9408d5bb0947ccb1929
Om <userpc6996@gmail.com>
2026-03-09 12:59:20 +0530

    basic maths from java.

--- math.java
+++ math.java
@@ -1,8 +1,12 @@
 public class math {
     public static void main(String[] args) {
-        System.out.println(Math.random());
+        int a= (int)(Math.random()*101);
+        System.out.println(a);
+        System.out.println(Math.random()*101);
         System.out.println(Math.min(5, 15));
         System.out.println(Math.max(5, 15));
         System.out.println(Math.sqrt(64));
+        System.out.println(Math.pow(2, 3));
+        
     }
 }
