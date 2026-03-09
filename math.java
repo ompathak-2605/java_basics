23eadcdb0665a94cc536e8ebb6b10eddd7b17e28
Om <userpc6996@gmail.com>
2026-03-09 13:06:08 +0530

    Some more added..

--- math.java
+++ math.java
@@ -7,6 +7,8 @@ public class math {
         System.out.println(Math.max(5, 15));
         System.out.println(Math.sqrt(64));
         System.out.println(Math.pow(2, 3));
-        
+        System.out.println(Math.abs(-67));
+        System.out.println(Math.addExact(2, 4));
+        System.out.println(Math.class);
     }
 }
