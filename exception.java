9e74b11f45fe089d902d2ef573f3a8f97489abef
Om <userpc6996@gmail.com>
2026-03-25 08:18:36 +0530

    Changed

--- exception.java
+++ exception.java
@@ -1,8 +1,13 @@
 public class exception {
-    public int checkindex(int arr[]){
-
-    }
     public static void main(String[] args) {
+        int[] arr = new int[10];
+        try {
+            System.out.println(arr[10]);
+        
+        } catch (IndexOutOfBoundsException e) {    
+           System.out.println("array's last index is 9."); 
+        }
         
     }
-}
+    }
+
